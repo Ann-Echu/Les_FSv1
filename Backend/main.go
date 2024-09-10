f7d7e32d95e3cd91c81b906b719c5826edc13dc1
William Nwoke <willteey@gmail.com>
2024-09-10 12:50:08 -0700

    decomposition

--- Backend/main.go
+++ Backend/main.go
@@ -1,1 +1,13 @@
-from 
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/api/recommendations", func(c *gin.Context) {
+		c.JSON(200, gin.H{})
+	})
+}
